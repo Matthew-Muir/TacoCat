481aba33c257dafd1aa606161a7b7869c154243c
Matt <matthewtmuir@gmail.com>
2021-07-26 21:30:36 -0700

    First Draft

--- js/site.js
+++ js/site.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function CheckString() {
+  let userStr = document.getElementById("textField").value.toLowerCase();
+  userStr = userStr.split(" ").join("");
+  let arr = userStr.split("");
+  arr = arr.reverse();
+  let revStr = arr.join("");
+  let resultForm = document.getElementById("result");
+  resultForm.classList.remove("invisible");
+  if (userStr == revStr) {
+    resultForm.getElementsByTagName("h2")[0].innerHTML = "Is a palindrome!";
+  } else {
+    resultForm.getElementsByTagName("h2")[0].innerHTML = "Not a palindrome!";
+  }
+}
+
+function Hide() {
+  document.getElementById("result").classList.add("invisible");
+}
